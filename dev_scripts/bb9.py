d7c6be0a6f28228750415228efd4a03cd2d312b1
lambertsbennett <ben@Bennetts-MacBook-Pro.local>
2019-02-04 13:12:33 -0800

    start with bb9 file read

--- dev_scripts/bb9.py
+++ dev_scripts/bb9.py
@@ -8,22 +8,23 @@ import numpy as np
 import os
 
 class bb9:
-"""
-The scaling factors are provided by the manufacturer and should be modified for your
-specific instrument.
+    """
+    The scaling factors are provided by the manufacturer and should be modified for your
+    specific instrument.
 
-data_path specifies the path to the bb9 .dat file
+    data_path specifies the path to the bb9 .dat file
 
-dark refers to the dark readings that should be carried out for calibration purposes
-"""
-    scaling_factors=np.array()
+    dark refers to the dark readings that should be carried out for calibration purposes
+    """
+
+    #scaling_factors=np.array()
     
-    def __init__(self):
+    def __init__(self,data=0,dark=0):
         self.data = data
         self.dark = dark
     
     def read_data(self,data_path):
-        self.data=pd.read_csv(data_path)
+        self.data=pd.read_csv(data_path,sep='\t',header=None)
         #need to structure this specifically for the bb9 .dat file
     
     def set_dark(self,dark):
@@ -32,4 +33,6 @@ dark refers to the dark readings that should be carried out for calibration purp
         self.dark = np.mean(dark_reading)
         #need to structure this specifically for the bb9 .dat file
 
-        
+
+#b=bb9()
+#b.read_data(data_path='/Users/ben/Documents/Optics-tk/example_data/bb9_example_file')
