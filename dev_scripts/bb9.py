d879b902a22fe7f1eb83c61ae60e3e78441fb532
lambertsbennett <ben@Bennetts-MacBook-Pro.local>
2019-02-04 13:42:49 -0800

    pffft

--- dev_scripts/bb9.py
+++ dev_scripts/bb9.py
@@ -18,20 +18,17 @@ class bb9:
     """    
     
     def __init__(self,data=0,dark=0,sc_f=np.array([4.533e-5,1.99e-5,1.946e-5,1.615e-5,1.682e-5,1.047e-5,9.738e-6,8.375e-6,6.404e-6])
-):
+):  #These scaling factors do not apply to your instrument!
         self.data = data
         self.dark = dark
         self.sc_f = sc_f
         
     def read_data(self,data_path):
         self.data=pd.read_csv(data_path,sep='\t',header=None)
-        #need to structure this specifically for the bb9 .dat file
     
     def set_dark(self,dark_path,dark_reading = np.zeros(9)):
-        #read the entire file and then return the average dark reading for each channel
         dark_reading = pd.read_csv(dark_path) #I will update this to read only the necessary columns
         self.dark = np.mean(dark_reading, axis=0)
-        #need to structure this specifically for the bb9 .dat file
 
 
 #b=bb9()
