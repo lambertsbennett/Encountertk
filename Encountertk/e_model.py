69cfb1ad104b2764d6ecdf723b9b3c270a6248d1
lambertsbennett <lambertsbennett@gmail.com>
2019-11-21 11:42:09 -0800

    particle functions

--- Encountertk/e_model.py
+++ Encountertk/e_model.py
@@ -4,6 +4,16 @@ Module to carry out common encounter rate calculations and modeling
 
 import numpy as np
 
+'''
+This file contains classes and functions related to calculating encounter rates and in the
+particle section, parameters associated with particle spectra.
+
+The class EncounterModel can serve as a base class to build increasingly complex encounter rate models
+and the values it's methods return are in a format that plays nicely with etk's associated plotting functions.
+
+Here and everywhere when making use of oceanographic data you need to be absolutely certain that the units are consistent.
+Everywhere possible I've tried to specifiy the units needed for constants and variables '''
+
 class EncounterModel():
     ''' Base class for encounter rate model.
     Requires: an encounter rate kernel value,
@@ -16,6 +26,12 @@ class EncounterModel():
         self.pop1c = pop1c
         self.pop2c = pop2c
 
+    def set_kernel(self,kernel):
+        self.kernel = kernel
+
+    def get_kernel(self):
+        return self.kernel
+
     def calc_encounter(self):
         ''' Calculate the encounter rate between two populations. The concentrations must
         be in list format. The encounter rate is returned as a list of arrays'''
@@ -27,6 +43,43 @@ class EncounterModel():
         self.e_rate = [self.kernel*np.multiply(self.pop2c,p1) for p1 in self.pop1c]
         return self.e_rate
 
+# ------------------------------------------------------------------------------------
+'''
+Functions dealing with particle spectra and encounter.
+'''
+
+from sympy import *
+
+#Using a symbolic math library makes this much more readable. I hope you would agree.
+r = symbols('r')
+
+'''
+This is the calculation used in Lambert et al. to caculate total encounter for a particle spectra.
+All length units must be in cm for this to work.
+rmin: minimum particle radius to consider (cm)
+rmax: max. particle radius to consider (cm)
+r0: reference particle diameter in psd power law (cm)
+B: psd slope (cm^-4)
+D: Diffusivity of bacterium (cm^2 s^-1)
+v: kinematic viscosity of water (roughly 0.01 cm^2 s^-1)
+Returns: search time in seconds.
+'''
+
+def ps_encounter(rmin,rmax,D,r0,N0,B,v):
+        expr = (4*N0*D*np.pi*r*(r/r0)**(-B))*(1+0.619*(((0.13*r**0.26)*r/v)**0.412)*(v/D)**(1/3))
+        e_total = integrate(expr,(r,rmin,rmax))
+        return e_total
+
+'''
+Mean volume encountered by a bacterium within a particle landscape. All parameters have the same definition as
+above.
+Returns: mean volume encountered in um
+'''
+
+def mean_vol_encountered(rmin,rmax,D,r0,N0,B,v):
+        expr = ((16/3)*N0*D*(np.pi**2)*(r**4)*(r/r0)**(-B)*(1+0.619*(((0.13*r**0.26)*r/v)**0.412)*(v/D)**(1/3))
+        mean_vol = integrate(expr,(r,rmin,rmax))
+        return mean_vol
 
 if __name__ == "__main__":
     print("Here's a toy example with two populations")
