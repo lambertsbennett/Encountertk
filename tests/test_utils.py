cdb61b54cfe29397d31320f45bcb8cec1bc6f41d
lambertsbennett <lambertsbennett@gmail.com>
2020-01-07 12:13:53 -0800

    util tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,32 @@
 from pytest import mark
 from encountertk._utils import *
+from numpy import isnan
 
 @mark.utils
-def test_diffusion():
-    pass
+class TestUtils:
+
+    def test_particle_diffusivity(self):
+        pass
+
+    def test_rw_diffusivity(self):
+        pass
+
+    def test_calculate_re(self):
+        pass
+
+    def test_calculate_pe(self):
+        pass
+
+    def test_sh_lessthan(self):
+        pass
+
+    def test_sh_greaterthan(self):
+        pass
+
+    def test_sh_undefined_highpe(self):
+        res = calculate_Sh(Re=1,Pe=100000,D=1,v=1)
+        assert isnan(res)
+
+    def test_sh_undefined_lowpe(self):
+        res = calculate_Sh(Re=1,Pe=10,D=1,v=1)
+        assert isnan(res)
