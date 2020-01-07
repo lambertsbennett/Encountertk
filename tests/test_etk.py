99a8e0c88ba47966178e962c3e2dee001a2ee1e5
lambertsbennett <lambertsbennett@gmail.com>
2020-01-07 12:00:15 -0800

    add particle encounter tests

--- tests/test_etk.py
+++ tests/test_etk.py
@@ -15,11 +15,19 @@ class TestEncounterModel:
         self.model.set_kernel(2.5)
         assert self.model.get_kernel() == 2.5
 
-    def test_calc_encounter_range(self):
-        assert True
+    def test_calc_encounter_range(self,EModel):
+        self.model = EModel
+        self.model.pop1c = [1,5,10]
+        self.model.pop2c = [2]
+        result = self.model.calc_encounter_range()
+        assert list(result) == [2,10,20]
 
-    def test_calc_encounter_pairwise(self):
-        assert True
+    def test_calc_encounter_pairwise(self,EModel):
+        self.model = EModel
+        self.model.pop1c = [1,5,10]
+        self.model.pop2c = [1,2,3]
+        result = self.model.calc_encounter_pairwise()
+        assert list(result) == [1,10,30]
 
 @mark.particles
 def test_ps_encounter():
---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+from pytest import mark
+from encountertk._utils import *
+
+@mark.utils
+def test_diffusion():
+    pass
